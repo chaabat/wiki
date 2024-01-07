20abc48f7235a3c4d02fc7c2fb38bac780d9ec64
chaabat <chaabat.02@gmail.com>
2024-01-07 13:30:16 +0100

    framework done

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,9 +1,27 @@
 <?php
+
+  session_start();
+
   // Load Config
   require_once 'config/config.php';
 
   // Autoload Core Libraries
-  spl_autoload_register(function($className){
-    require_once 'libraries/' . $className . '.php';
-  });
-  
+  function Autoloader($class) {
+    $paths = [
+        APPROOT."/libraries/",
+        APPROOT."/services/interfaces/",
+        APPROOT."/services/implementations/",
+        APPROOT."/models/"
+    ];
+
+
+    foreach ($paths as $path) {
+        $file = $path . $class . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    }
+}
+
+
+spl_autoload_register('Autoloader');
