e6057ac6b824bae325c453576f9b6a354d9208e3
chaabat <chaabat.02@gmail.com>
2024-01-07 13:16:00 +0100

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'wiki');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/wiki');
+  // Site Name
+  define('SITENAME', 'Wiki');
